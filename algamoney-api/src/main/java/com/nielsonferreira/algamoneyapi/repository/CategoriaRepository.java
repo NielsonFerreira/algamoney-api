d83bd58cb696976af94b5eeeaf7d0059c1260275
Nielson Ferreira <nielsonfferreira@hotmail.com>
2020-06-24 23:30:30 -0300

    3.4 Consultando primeiro recurso com GET

---
+++ algamoney-api/src/main/java/com/nielsonferreira/algamoneyapi/repository/CategoriaRepository.java
@@ -0,0 +1,9 @@
+package com.nielsonferreira.algamoneyapi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.nielsonferreira.algamoneyapi.model.Categoria;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long>{
+
+}
