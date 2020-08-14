8bc9744e7fc0784ac11f4ef318d8cf5be0c5c435
Nielson Ferreira <nielsonfferreira@hotmail.com>
2020-08-14 13:35:00 -0300

    Retornar 404 caso não exista a categoria/Validando atributos desconhecidos/Tratando erros com ExceptionHandler

--- algamoney-api/src/main/java/com/nielsonferreira/algamoneyapi/repository/CategoriaRepository.java
+++ algamoney-api/src/main/java/com/nielsonferreira/algamoneyapi/repository/CategoriaRepository.java
@@ -1,9 +1,10 @@
 package com.nielsonferreira.algamoneyapi.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
 import com.nielsonferreira.algamoneyapi.model.Categoria;
 
-public interface CategoriaRepository extends JpaRepository<Categoria, Long>{
+public interface CategoriaRepository extends JpaRepository<Categoria, Long>, CrudRepository<Categoria, Long>{
 
 }
